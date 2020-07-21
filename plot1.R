fbb46a3e8af13c6ad7ab9a1ac761824801d98b59
hilda-k <66512718+hilda-k@users.noreply.github.com>
2020-07-21 08:24:09 +0800

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -1,21 +1,30 @@
-# Must be run from directory containing household_power_consumption.txt
-datafile <- "household_power_consumption.txt"
-if (!file.exists(datafile)) stop(paste("Didn't find data file", datafile))
+require(data.table)
 
-# The data is in a ; separated file with ? for NA
-col.class <- c("character", "character", "numeric", "numeric", "numeric", "numeric",
-              "numeric", "numeric", "numeric")
-data <- read.table(datafile, sep=";", header=TRUE,
-                   colClasses=col.class, na.strings="?",
-                   nrows=2075260)
-data$Date <- as.Date(data$Date, format="%d/%m/%Y")
+### Download & unzip data
+dataURL <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+download.file(dataURL, "./tmp.zip")
+unzip("./tmp.zip")
+file.remove("./tmp.zip")
 
-# select desired dates and variable
-data <- subset(data, Date=="2007-02-01" | Date=="2007-02-02",
-               Global_active_power)
+### Read data
+# Get headers (because skip used later)
+header <- scan("./household_power_consumption.txt", what = character(), nlines = 1, quiet = TRUE)
+header <- strsplit(header, ";")
 
-# Histogram
-png(filename="plot1.png", width=480, height=480)
-hist(data$Global_active_power, xlab="Global Active Power (kilowatts)",
-     col="red", main="Global Active Power")
+# Read data with filter those w/ desired dates
+data <- fread("./household_power_consumption.txt", sep=";", header=FALSE, na.strings="?", skip = 66600, nrows = 4000)
+setnames(data, header[[1]])
+remove(header)
+data <- subset(data, Date == '1/2/2007' | Date == '2/2/2007')
+
+# Merge date & time into single column
+dateTime <- as.POSIXct(paste(data$Date, data$Time, sep = ";"), format = "%d/%m/%Y;%H:%M:%S")
+data$Date <- NULL
+data$Time <- NULL
+data <- cbind("DateTime" = dateTime, data)
+remove(dateTime)
+
+### Plot graph
+png(filename = "plot1.png", width = 480, height = 480, units = "px", bg = "transparent")
+hist(data$Global_active_power, col = "red", main = "Global Active Power", xlab = "Global Active Power (kilowatts)")
 dev.off()
